08d87ecb233763bd50dc6b0568e121bb7e616254
TiagodSobreira <49566651+TiagodSobreira@users.noreply.github.com>
2019-10-14 15:22:06 -0300

    Update python_snake_game.py

--- python_snake_game.py
+++ python_snake_game.py
@@ -1,76 +1,75 @@
-#jogo da cobrinha 
-#duas classes: cobra e cubo
-import pygame
+#snake game in python using turtle model
+import pygame  
+from pygame.locals import *
 import random
 
-#criando classe dos cubos
-#são retângulos que possuem uma posição e uma cor
-class Cubo:
-    def __init__(self,color,pos):
-        self.color = color
-        self.pos = pos
-        
-    def draw_cubo(self,surface):
-        pygame.draw.rect(surface, self.color, (self.pos[0], self.pos[1], widght, height))
-    
-#função que redesenha a tela do jogo, após todas as alterações terem isso feitas
-def redrawthegameWindow():
-    win.fill((0,0,0))
-    win.blit(bg,(0,0))
-    snake_skin = Cubo((255,0,0),(head_x,head_y))
-    snake_skin.draw_cubo(win)
-    maça = Cubo((153,204,50),(maça_x,maça_y))
-    maça.draw_cubo(win)
-    pygame.display.update()
+def on_grid_random():
+    x = random.randint(0,590)
+    y = random.randint(0,590)
+    return (x//10 * 10, y//10 * 10)
 
-#carregando imagem de fundo do jogo
-bg = pygame.image.load('./Downloads/Escritório_FEA.dev/Game/bg.jpg') 
-#velocidade com que o cubo vermelho se move
-vel = 10
-#tamamho da tela
-width = 600
-#comprimento e largura dos cudos, são usados na função draw_cubo
-height = 20
-widght = 20
-#posição da cabeça da cobra (cubo vermelho que se move)
-head_x = 10
-head_y = 10
-#a posição inicial da Maça também vai ser aleatória
-maça_x = random.randint(0,460)
-maça_y = random.randint(0,460)
-#criando uma relógio no pygame que controla o fps máximo
+def collision(c1, c2):
+    return (c1[0] == c2[0]) and (c1[1] == c2[1] )
+
+#criando variaveis que indicam a direção que a cobra está se movendo 
+UP = 0
+RIGHT = 1
+DOWN = 2
+LEFT = 3
+
+
+pygame.init()
+
+screen = pygame.display.set_mode((600,600))
+pygame.display.set_caption("Snake")
+
+#criando uma snake
+snake = [(200,200),(210,200),(220,200)]
+snake_skin = pygame.Surface((10,10))
+snake_skin.fill((255,255,255))
+
+apple = pygame.Surface((10,10))
+apple.fill((255,0,0))
+apple_pos = (on_grid_random())
+
+my_direction = LEFT
 clock = pygame.time.Clock()
 
-
-#iniciando pygame
-pygame.init()
-#criando janela do jogo
-win = pygame.display.set_mode((width,width))
-#criando título para essa janela 
-pygame.display.set_caption("Snake game in python")
-
-#loop principal onde ocorre o jogo
-finished = False
-while finished == False:
+while True:
     clock.tick(20)
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
-            finished = True
-    keys = pygame.key.get_pressed()
-    if keys[pygame.K_LEFT]:
-        head_x -=vel
-    if keys[pygame.K_RIGHT]:
-        head_x += vel
-    if keys[pygame.K_UP]:
-        head_y -=vel    
-    if keys[pygame.K_DOWN]:
-        head_y += vel
-    #cria limites para os movimentos do cubo vermelho
-    if head_x <= 0 or head_x >= width:
-        finished = True
-    if head_y <=0 or head_y >= width:
-        finished = True
+            pygame.quit()
+            print("Sua pontuação foi",len(snake)-3)
+        
+        if event.type == KEYDOWN:
+            if event.key == K_UP:
+                my_direction = UP
+            if event.key == K_DOWN:
+                my_direction = DOWN
+            if event.key == K_RIGHT:
+                my_direction = RIGHT
+            if event.key == K_LEFT:
+                my_direction = LEFT
+    if collision(snake[0], apple_pos):
+        apple_pos = on_grid_random()
+        snake.append((0,0))
     
-    redrawthegameWindow()
-#saindo do pygame
-pygame.quit()
+    for i in range(len(snake) - 1, 0, -1):
+        snake[i] = (snake[i-1][0], snake[i-1][1])
+
+    if my_direction == UP:
+        snake[0] = (snake[0][0], snake[0][1] - 5)
+    if my_direction == DOWN:
+        snake[0] = (snake[0][0], snake[0][1] + 5)
+    if my_direction == RIGHT:
+        snake[0] = (snake[0][0] + 5, snake[0][1])
+    if my_direction == LEFT:
+        snake[0] = (snake[0][0] - 5 , snake[0][1])
+    
+    screen.fill((0,0,0))
+    screen.blit(apple, apple_pos)
+    for pos in snake:
+        screen.blit(snake_skin,pos)
+    
+    pygame.display.update()
